c8eaa2525385555637a5102ef748839d0e390e75
Ray <megrxu@gmail.com>
2017-09-06 12:08:43 +0800

    adjust

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests, json
-
-news_json = requests.get('https://newsapi.org/v1/sources?language=en')
-news_json = news_json.text
-print(news_json)
-news_object = json.loads(news_json)
-print(news_json)
-for item in news_object['sources']:
-  print(item['url'])
